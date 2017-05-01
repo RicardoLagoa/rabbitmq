81e8a33775ca480deb3d759e10f137ba38192f1a
RicardoLagoa <lagoa82@gmail.com>
2017-05-01 16:08:51 +0100

    rabbitmq -tutorial

---
+++ send.php
@@ -0,0 +1,20 @@
+<?php
+error_reporting( E_ALL);
+ini_set("display_errors",true);
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+$channel->queue_declare('hello', false, false, false, false);
+
+$msg = new AMQPMessage('Hello World!');
+$channel->basic_publish($msg, '', 'hello');
+
+echo " [x] Sent 'Hello World!'\n";
+
+$channel->close();
+$connection->close();
